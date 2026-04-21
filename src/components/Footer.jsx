8aa8d8c0c516b38df5318a4b3af585637dface6d
Say my name <moresanket2305@gmail.com>
2026-04-21 20:09:30 +0530

    changes ec2 so testing jenkins again2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = (props) => {
       <div className='bgGradient'></div>
       <div>
             <h2>{data?.title}</h2>
-            <h1>APOD PROJECT</h1>
+            <h1>APOD PROJECT by sanku</h1>
             
       </div>
         
